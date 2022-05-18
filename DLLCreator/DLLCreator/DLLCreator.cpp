494fad1ed45c3f7214283f040a264f99bd947253
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-05-18 10:22:12 +0200

    made constructor and Convert()

--- DLLCreator/DLLCreator/DLLCreator.cpp
+++ DLLCreator/DLLCreator/DLLCreator.cpp
@@ -2,5 +2,13 @@
 
 namespace DLL
 {
+	DLLCreator::DLLCreator(const std::string& rootPath)
+		: RootPath(rootPath)
+	{}
 
+	void DLLCreator::Convert()
+	{
+		/* Step 1: Get all files in this root directory */
+		
+	}
 }
--- DLLCreator/DLLCreator/DLLCreator.h
+++ DLLCreator/DLLCreator/DLLCreator.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <string>
+
 namespace DLL
 {
 	class DLLCreator final
 	{
+	public:
+		DLLCreator(const std::string& rootPath);
+
+		void Convert();
+
+	private:
+		std::string RootPath;
 	};
 }
