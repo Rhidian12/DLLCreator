5fe3529c672c843352d836d6290b1575b726eb26
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-05-21 14:03:19 +0200

    added multiple helper functions
    StringFind
    StringLength return type changed
    Added BYTE StringCompare and StringContains overload

--- DLLCreator/Utils/Utils.cpp
+++ DLLCreator/Utils/Utils.cpp
@@ -3,6 +3,8 @@
 #include <iostream> /* std::cin */
 #include <assert.h> /* assert() */
 
+#undef max
+
 namespace Utils
 {
 	namespace IO
@@ -74,6 +76,38 @@ namespace Utils
 			return 0;
 		}
 
+		int StringCompare(const BYTE* const pString, const BYTE* const pOtherString, const char delimiter)
+		{
+			assert(pString != nullptr);
+			assert(pOtherString != nullptr);
+
+			int counter{};
+			while (true)
+			{
+				if (pString[counter] != '\0' && pString[counter] != delimiter)
+				{
+					if (pOtherString[counter] == '\0' || pOtherString[counter] == delimiter)
+					{
+						return 1;
+					}
+
+					if (pOtherString[counter] > pString[counter])
+					{
+						return -1;
+					}
+
+					if (pString[counter] > pOtherString[counter])
+					{
+						return 1;
+					}
+				}
+
+				++counter;
+			}
+
+			return 0;
+		}
+
 		/* https://stackoverflow.com/questions/18680118/own-implementation-of-stdstringfind-brute-force-search */
 		bool StringContains(const char* const pString, const char* const pOtherString, const char delimiter)
 		{
@@ -101,9 +135,35 @@ namespace Utils
 			return false;
 		}
 
-		int StringLength(const char* const pString, const char delimiter)
+		bool StringContains(const BYTE* const pString, const BYTE* const pOtherString, const char delimiter)
 		{
-			int counter{};
+			assert(pString != nullptr);
+			assert(pOtherString != nullptr);
+
+			int stringLen = StringLength(pString, delimiter);
+			int otherStringLen = StringLength(pOtherString, delimiter);
+
+			for (int i = 0; i <= stringLen - otherStringLen; ++i)
+			{
+				int j{};
+				while (j < otherStringLen && pString[i + j] == pOtherString[j])
+				{
+					++j;
+				}
+
+				/* match found */
+				if (j == otherStringLen)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		size_t StringLength(const char* const pString, const char delimiter)
+		{
+			size_t counter{};
 			while (pString[counter] != delimiter)
 			{
 				++counter;
@@ -112,9 +172,9 @@ namespace Utils
 			return counter;
 		}
 
-		int StringLength(const BYTE* const pString, const char delimiter)
+		size_t StringLength(const BYTE* const pString, const char delimiter)
 		{
-			int counter{};
+			size_t counter{};
 			while (pString[counter] != delimiter)
 			{
 				++counter;
@@ -128,7 +188,6 @@ namespace Utils
 			assert(pDest != nullptr);
 			assert(pSource != nullptr);
 
-			int x{ StringLength(pSource, '\n') };
 			assert(StringLength(pSource, '\n') >= count);
 
 			for (size_t i{}; i < count; ++i)
@@ -144,7 +203,6 @@ namespace Utils
 			assert(pDest != nullptr);
 			assert(pSource != nullptr);
 
-			int x{ StringLength(pSource, '\n') };
 			assert(StringLength(pSource, '\n') >= count);
 
 			for (size_t i{}; i < count; ++i)
@@ -153,6 +211,53 @@ namespace Utils
 			}
 
 			return true;
+		}
+
+		DWORD StringFind(const BYTE* pDest, const BYTE* pOtherString, const BYTE delimiter)
+		{
+			assert(pDest != nullptr && "Utils::IO::StringFind() > pDest is a nullptr");
+			assert(pOtherString != nullptr && "Utils::IO::StringFind() > pOtherString is a nullptr");
+
+			const size_t strLen(StringLength(pDest, delimiter));
+			const size_t otherStrLen(StringLength(pOtherString, delimiter));
+
+			assert(strLen >= otherStrLen && "Utils::IO::StringFind() > pDest must be larger or equal to pOtherString!");
+
+			for (size_t i{}; i < strLen; ++i)
+			{
+				if (pDest[i] != delimiter)
+				{
+					if (pDest[i] == pOtherString[i])
+					{
+						if (StringCompare(pDest, pOtherString, delimiter) == 0)
+						{
+							return static_cast<DWORD>(i);
+						}
+					}
+				}
+			}
+
+			return std::numeric_limits<DWORD>::max();
+		}
+
+		DWORD StringFind(const BYTE* pDest, const BYTE charToFind, const BYTE delimiter)
+		{
+			assert(pDest != nullptr && "Utils::IO::StringFind() > pDest is a nullptr");
+
+			const size_t strLen(StringLength(pDest, delimiter));
+
+			for (size_t i{}; i < strLen; ++i)
+			{
+				if (pDest[i] != delimiter)
+				{
+					if (pDest[i] == charToFind)
+					{
+						return static_cast<DWORD>(i);
+					}
+				}
+			}
+
+			return std::numeric_limits<DWORD>::max();
 		}
 	}
 }
--- DLLCreator/Utils/Utils.h
+++ DLLCreator/Utils/Utils.h
@@ -17,10 +17,18 @@ namespace Utils
 
 		/* 0 means equal, 1 means pString > pOtherString, -1 means pOtherString > pString */
 		int StringCompare(const char* const pString, const char* const pOtherString, const char delimiter);
+		int StringCompare(const BYTE* const pString, const BYTE* const pOtherString, const char delimiter);
+		
 		bool StringContains(const char* const pString, const char* const pOtherString, const char delimiter);
-		int StringLength(const char* const pString, const char delimiter);
-		int StringLength(const BYTE* const pString, const char delimiter);
+		bool StringContains(const BYTE* const pString, const BYTE* const pOtherString, const char delimiter);
+
+		size_t StringLength(const char* const pString, const char delimiter);
+		size_t StringLength(const BYTE* const pString, const char delimiter);
+
 		bool StringCopy(char* pDest, const char* pSource, const size_t count);
 		bool StringCopy(BYTE* pDest, const BYTE* pSource, const size_t count);
+
+		DWORD StringFind(const BYTE* pDest, const BYTE* pOtherString, const BYTE delimiter);
+		DWORD StringFind(const BYTE* pDest, const BYTE charToFind, const BYTE delimiter);
 	}
 }
